e1e31d547d09f864667b4830c630d85a58eaafbc
Taylor McCarthy <oresshi@gmail.com>
2017-07-27 11:13:48 -0600

    fixed jquery/bootstrap load order

--- public_html/devcal.php
+++ public_html/devcal.php
@@ -5,6 +5,10 @@
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+		<!-- jquery scripts -->
+		<script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- Optional theme -->
@@ -14,7 +18,31 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
+
+		<!-- moment.js scripts -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+		<!-- FullCalendar scripts -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css">
+		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.print.css">
+
+
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
+		<script type='text/javascript'>
+
+			$(document).ready(function() {
+				$('#calendar').fullCalendar({
+					googleCalendarApiKey: 'AIzaSyAlc7zJWBH4IV3qXTv_XUQwJCUHZPMS4wI',
+					events: {
+						googleCalendarId: 'h469sf6pp54j90dieounarv9vk@group.calendar.google.com'
+					}
+				});
+			});
+
+		</script>
 	</head>
 	<body>
 		<header>
@@ -28,26 +56,6 @@
 				</div>
 				<!-- End Banner -->
 		</header>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for a better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle Navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Brand</a>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home<span class="sr-only"></span></a></li>
-						<li><a href="#">Schedules</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<div id="calendar"></div>
 	</body>
 </html>
